ae366774573501f7229a64086c1c840b5df622b5
Cleve-keem <belloharyourmidey@gmail.com>
2025-01-21 12:05:08 -0800

    added the fetch feature and loading the item from the array

--- script.js
+++ script.js
@@ -16,57 +16,59 @@ placeOrderBtn.addEventListener("click", toggleOrderNotification);
 overlay.addEventListener("click", toggleOrderNotification);
 startNewOrderbtn.addEventListener("click", toggleOrderNotification);
 
-const addToCartBtn = document.querySelectorAll(".add-to-cart-btn");
-const itemContainer = document.querySelectorAll("item-container");
-const productList = document.querySelector(".product-items-list");
-const listOfProducts = [];
+let listOfProducts = []; // creating an array
 
 async function fetchProductItems() {
-  const res = await fetch("data.json");
-  const data = await res.json();
-  console.log(data);
-  listOfProducts.push(data);
-  // addProductsToHTML();
+  try {
+    const response = await fetch("data.json");
+    const data = await response.json();
+    listOfProducts = data;
+  } catch (error) {
+    console.log("Error fetching data: ", error);
+  }
+  addProductsToHTML();
+}
+fetchProductItems();
+
+// console.log(listOfProducts)
+
+const productList = document.querySelector(".product-items-list");
+
+function addProductsToHTML() {
+  productList.innerHTML = "";
+  if (listOfProducts.length > 0) {
+    listOfProducts.forEach((product, index) => {
+      console.log(product);
+      const itemContainer = document.createElement("div");
+      itemContainer.classList.add("item-container");
+      itemContainer.innerHTML = `<div class="item-view">
+                  <img
+                    class="item-img"
+                    src="${product[index]?.images.thumbnail}"
+                    alt=""
+                  />
+                  <button class="add-to-cart-btn">
+                    <img
+                      class="icon"
+                      src="./assets/images/icon-add-to-cart.svg"
+                      alt="cart icon"
+                    />
+                    Add to Cart
+                  </button>
+                </div>
+                <div class="desc">
+                  <p class="item-faded-name">Waffle</p>
+                  <h2 class="item-name">Waffle with Berries</h2>
+                  <span class="price">$6.50</span>
+                </div>`;
+    });
+  }
 }
 
-// function addProductsToHTML() {
-//   productList.innerHTML = "";
-//   // console.log(listOfProducts);
-//   if (listOfProducts > 0) {
-//     listOfProducts.forEach((product, index) => {
-//       console.log(product);
-//       const itemContainer = document.createElement("div");
-//       itemContainer.classList.add("item-container");
-//       itemContainer.innerHTML = `<div class="item-view">
-//                   <img
-//                     class="item-img"
-//                     src="${product[index]?.images.thumbnail}"
-//                     alt=""
-//                   />
-//                   <button class="add-to-cart-btn">
-//                     <img
-//                       class="icon"
-//                       src="./assets/images/icon-add-to-cart.svg"
-//                       alt="cart icon"
-//                     />
-//                     Add to Cart
-//                   </button>
-//                 </div>
-//                 <div class="desc">
-//                   <p class="item-faded-name">Waffle</p>
-//                   <h2 class="item-name">Waffle with Berries</h2>
-//                   <span class="price">$6.50</span>
-//                 </div>`;
-//     });
-//   }
-// }
-
-productList.addEventListener("click", (event)=>{
-  let selectedTagName = event.target.tagName; 
-  if(selectedTagName === "BUTTON"){
+productList.addEventListener("click", (event) => {
+  let selectedTagName = event.target.tagName;
+  if (selectedTagName === "BUTTON") {
     const parentContainer = event.target.parentElement;
     parentContainer.classList.add(ACTIVE_CLASS);
   }
 });
-
-fetchProductItems();
