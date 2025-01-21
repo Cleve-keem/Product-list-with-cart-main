bf5d2e85befbae63816774979ddc04869e81701a
Cleve-keem <belloharyourmidey@gmail.com>
2025-01-21 09:57:19 -0800

    adding border to selected items

--- script.js
+++ script.js
@@ -17,19 +17,56 @@ overlay.addEventListener("click", toggleOrderNotification);
 startNewOrderbtn.addEventListener("click", toggleOrderNotification);
 
 const addToCartBtn = document.querySelectorAll(".add-to-cart-btn");
-const itemContainer = document.querySelectorAll(".item-container");
+const itemContainer = document.querySelectorAll("item-container");
+const productList = document.querySelector(".product-items-list");
+const listOfProducts = [];
 
-console.log(itemContainer);
-console.log(addToCartBtn);
-
-function selectedItem() {
-  addToCartBtn.forEach((button, index) => {
-    if(itemContainer[index]){
-      button.addEventListener("click", ()=>{
-        itemContainer[index].classList.add(ACTIVE_CLASS);
-      })
-    }
-  });
+async function fetchProductItems() {
+  const res = await fetch("data.json");
+  const data = await res.json();
+  console.log(data);
+  listOfProducts.push(data);
+  // addProductsToHTML();
 }
 
-selectedItem();
+// function addProductsToHTML() {
+//   productList.innerHTML = "";
+//   // console.log(listOfProducts);
+//   if (listOfProducts > 0) {
+//     listOfProducts.forEach((product, index) => {
+//       console.log(product);
+//       const itemContainer = document.createElement("div");
+//       itemContainer.classList.add("item-container");
+//       itemContainer.innerHTML = `<div class="item-view">
+//                   <img
+//                     class="item-img"
+//                     src="${product[index]?.images.thumbnail}"
+//                     alt=""
+//                   />
+//                   <button class="add-to-cart-btn">
+//                     <img
+//                       class="icon"
+//                       src="./assets/images/icon-add-to-cart.svg"
+//                       alt="cart icon"
+//                     />
+//                     Add to Cart
+//                   </button>
+//                 </div>
+//                 <div class="desc">
+//                   <p class="item-faded-name">Waffle</p>
+//                   <h2 class="item-name">Waffle with Berries</h2>
+//                   <span class="price">$6.50</span>
+//                 </div>`;
+//     });
+//   }
+// }
+
+productList.addEventListener("click", (event)=>{
+  let selectedTagName = event.target.tagName; 
+  if(selectedTagName === "BUTTON"){
+    const parentContainer = event.target.parentElement;
+    parentContainer.classList.add(ACTIVE_CLASS);
+  }
+});
+
+fetchProductItems();
